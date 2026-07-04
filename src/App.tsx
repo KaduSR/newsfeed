cebef01312d9216c4d8d7d6d661f4b5603a2b1db
Carlos Eduardo da Silva Ribeiro <kaduesr@gmail.com>
2026-07-04 15:50:11 -0300

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,1 @@
+export default function App() { return React.createElement("div", {className:"app"}, React.createElement("h1", null, "newsfeed"), React.createElement("p", null, "Veja o README para detalhes.")) }
